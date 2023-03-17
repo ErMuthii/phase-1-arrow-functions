3afefdf34eb005c3def8739fea7e596d329c7949
Mary Wanjiku <er.muthii@gmail.com>
2023-03-17 22:12:40 +0300

    COmpleted assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(value1=2000,value2=100){
+    return(value1/value2)
+
+}
+divide()
+
+const square= (value3)=> value3*value3;
+square(3,3);
+
+const add= (value4,value5)=> value4+value5
+add(18,6)
